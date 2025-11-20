f684d23d554943920447ba913941802b52d61ad9
alempeco <aleem97peco@gmail.com>
2025-11-20 23:08:23 +0100

    Rute marge

--- frontend/elibery-frontend/src/app/app.routes.ts
+++ frontend/elibery-frontend/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './guards/auth.guard';
+import { RegisterComponent } from './register/register.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
